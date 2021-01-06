c5dd13a62d63c13a34e34ebde21ac99cd837d082
N0FreeLunch <consilience90@gmail.com>
2021-01-06 23:57:59 +0900

    overflow

---
+++ 03_operator/05_noUnderflow.go
@@ -0,0 +1,19 @@
+package main
+import "fmt"
+
+func main() {
+  var num1 uint8 = 0
+  var num2 uint16 = 0
+  var num3 uint32 = 0
+  var num4 uint64 = 0
+
+  num1--
+  num2--
+  num3--
+  num4--
+
+  fmt.Println(num1)
+  fmt.Println(num2)
+  fmt.Println(num3)
+  fmt.Println(num4)
+}
