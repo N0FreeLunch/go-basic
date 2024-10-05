f6c8a66ce3d19e057e332b872d843830e8cca176
N0FreeLunch <consilience90@gmail.com>
2024-10-05 23:38:49 +0900

    タイプの説明追加

--- 02_type/byte/byte.go
+++ 02_type/byte/byte.go
@@ -1,11 +1,12 @@
 package main
+
 import "fmt"
 
-func main () {
-  var num1 byte = 10
-  var num2 byte = 0x32
-  var num3 byte = 'a'
-  fmt.Println(num1)
-  fmt.Println(num2)
-  fmt.Println(num3)
+func main() {
+	var num1 byte = 10
+	var num2 byte = 0x32
+	var num3 byte = 'a'
+	fmt.Println(num1)
+	fmt.Println(num2)
+	fmt.Println(num3)
 }
--- 02_type/equalFloat/equalFloat.go
+++ 02_type/equalFloat/equalFloat.go
@@ -9,7 +9,7 @@ func main() {
 		a = a - 0.1
 	}
 
-	fmt.Println(a)        // 9.000000000000004: 반올림 오차 발생
-	fmt.Println(a == 9.0) // false: 실수는 ==로 비교하면 안됨
+	fmt.Println(a)        // 9.000000000000004: 四捨五入（ししゃごにゅう）誤差発生
+	fmt.Println(a == 9.0) // false: 実数は==で比較するとダメです。
 
 }
