34c84443fcffec6e291db0aabcd408629b70fa71
N0FreeLunch <consilience90@gmail.com>
2024-10-07 08:29:54 +0900

    バイトタイプの説明追加

--- 02_type/byteError/byteError.go
+++ 02_type/byteError/byteError.go
@@ -1,11 +1,12 @@
 package main
+
 import "fmt"
 
-func main () {
-  var num1 byte = "a"
-  var num2 byte = 'ab'
-  var num3 byte = 'ab'
-  fmt.Println(num1)
-  fmt.Println(num2)
-  fmt.Println(num3)
+func main() {
+	var num1 byte = 'a'
+	// var num2 byte = "ab" // error
+	// var num3 byte = 'ab' // error
+	fmt.Println(num1)
+	// fmt.Println(num2) // error
+	// fmt.Println(num3) // error
 }
