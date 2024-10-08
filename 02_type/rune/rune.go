cd2281bc1b88f72964d1192eebfb0f9fa8fec7d4
N0FreeLunch <consilience90@gmail.com>
2024-10-08 23:23:49 +0900

    runeタイプの説明追加

--- 02_type/rune/rune.go
+++ 02_type/rune/rune.go
@@ -1,12 +1,13 @@
 package main
+
 import "fmt"
 
-func main () {
-  var r1 rune = '한'
-  var r2 rune = '\ud55c'
-  var r3 rune = '\U0000d55c'
+func main() {
+	var r1 rune = '한'
+	var r2 rune = '\ud55c'
+	var r3 rune = '\U0000d55c'
 
-  fmt.Println(r1)
-  fmt.Println(r2)
-  fmt.Println(r3)
+	fmt.Println(r1)
+	fmt.Println(r2)
+	fmt.Println(r3)
 }
--- 02_type/runeError/runeError.go
+++ 02_type/runeError/runeError.go
@@ -1,12 +1,11 @@
 package main
-import "fmt"
 
-func main () {
-  var r1 rune = "한"
-  var r2 rune = '한글'
-  var r3 rune = "한글"
+func main() {
+	// var r1 rune = "한"
+	// var r2 rune = '한글'
+	// var r3 rune = "한글"
 
-  fmt.Println(r1)
-  fmt.Println(r2)
-  fmt.Println(r3)
+	// fmt.Println(r1)
+	// fmt.Println(r2)
+	// fmt.Println(r3)
 }
