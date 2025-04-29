36ad9f91266d4191b048fd6baf9d4032d4891e90
N0FreeLunch <consilience90@gmail.com>
2025-04-29 23:11:35 +0900

    文字列の数をカウント

--- 04_string/length/02-length.go
+++ 04_string/length/02-length.go
@@ -1,17 +1,21 @@
 package main
 
-import "fmt"
-import "unicode/utf8"
+import (
+	"fmt"
+	"unicode/utf8"
+)
 
-func main () {
-  var s1 string = "태스트"
-  var s2 string = "test"
+func main() {
+	var s1 string = "태스트"
+	var s2 string = "test"
 
-  fmt.Print("byte length : ")
-  fmt.Println(len(s1))
-  fmt.Print("byte length : ")
-  fmt.Println(len(s2))
-  fmt.Println(utf8.RuneCountInString(s1))
-  fmt.Println(utf8.RuneCountInString(s2))
+	fmt.Printf("byte length of \"%s\" : ", s1)
+	fmt.Println(len(s1))
+	fmt.Printf("byte length of \"%s\" : ", s2)
+	fmt.Println(len(s2))
+	fmt.Printf("letter length of \"%s\" : ", s1)
+	fmt.Println(utf8.RuneCountInString(s1))
+	fmt.Printf("letter length of \"%s\" : ", s2)
+	fmt.Println(utf8.RuneCountInString(s2))
 
 }
